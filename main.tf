f83acdb23a0a0c4161fd0a70c1148eb966b2d331
unknown <santhoshsai80@gmail.com>
2022-09-17 20:59:38 +0530

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.36.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "terraform-practice2"
+  region = "us-central1"
+  zone = "us-central1-a"
+}
+
+
 resource "google_cloud_run_service" "test" {
   name = "cloud-run"
     location = "asia-southeast1"
-    project = "terraform-practice2"
+    #project = "terraform-practice2"
     template {
       spec {
         containers {
