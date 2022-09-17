2ac6a8997c99cd571c4547a5ec38d552baaee4e1
unknown <santhoshsai80@gmail.com>
2022-09-17 20:49:45 +0530

    update

--- main.tf
+++ main.tf
@@ -4,11 +4,9 @@ resource "google_cloud_run_service" "test" {
     project = "terraform-practice2"
     region = "us-central1"
     zone = "us-central1-a"
-
-    terraform {
-      backend "gcs" {
-        bucket  = "santhoshstate"
-        prefix  = "terraform/state"
+    backend "gcs" {
+      bucket  = "santhoshstate"
+      prefix  = "terraform/state"
  }
 }
 
