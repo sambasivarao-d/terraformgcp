13a6fb44c9aed9b8204810c5c4fa114b357ac745
unknown <santhoshsai80@gmail.com>
2022-09-17 20:48:22 +0530

    update

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ resource "google_cloud_run_service" "test" {
   name = "cloud-run"
     location = "asia-southeast1"
     project = "terraform-practice2"
+    region = "us-central1"
+    zone = "us-central1-a"
 
     terraform {
       backend "gcs" {
