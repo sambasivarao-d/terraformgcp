605a649c3ae5bf0d222d3ad44f054b4efd9dec05
unknown <santhoshsai80@gmail.com>
2022-09-17 20:54:46 +0530

    update

--- main.tf
+++ main.tf
@@ -4,16 +4,18 @@ resource "google_cloud_run_service" "test" {
     project = "terraform-practice2"
     region = "us-central1"
     zone = "us-central1-a"
+    template {
+      spec {
+        containers {
+          image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+      }
+      }
     backend "gcs" {
       bucket  = "santhoshstate"
       prefix  = "terraform/state"
  }
-}
 
-template {
-  spec {
-    containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
-      }
-      }
+
+
       }
