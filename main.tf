7f6708d0bbc3384021eeb885e754c1d9c16bcf24
santhosh <santhoshsai80@gmail.com>
2022-09-19 23:05:07 +0530

    update

--- main.tf
+++ main.tf
@@ -22,22 +22,7 @@ resource "google_bigquery_dataset" "dataset" {
 resource "google_bigquery_table" "table" {
     table_id = "testtable"
     dataset_id = google_bigquery_dataset.dataset.dataset_id
-    schema =  <<EOF
-[
-  {
-    "name": "permalink",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "The Permalink"
-  },
-  {
-    "name": "state",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "State where the head office is located"
-  }
-]
-EOF
+    schema =  "schema.json"
 
 
   
